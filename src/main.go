0a4bbd448d7b6195b07de3afa605db2c4eaef2c1
Shuhei-pp <lotakotayota@gmail.com>
2022-08-29 22:16:40 +0900

    corsでfrontが許可されていなかったので修正

--- src/main.go
+++ src/main.go
@@ -3,12 +3,28 @@ package main
 import (
     "todo/controller"
     "github.com/gin-gonic/gin"
+    "github.com/gin-contrib/cors"
 )
 
 func main() {
     router := gin.Default()
     router.LoadHTMLGlob("templates/*")
     router.Static("/assets","assets")
+
+    //api投げたらcorsで引っかかったので
+    router.Use(cors.New(cors.Config{
+        AllowOrigins: []string{
+            "http://localhost:3000",
+        },
+        AllowMethods: []string{
+            "POST",
+            "GET",
+        },
+        AllowHeaders: []string{
+            "Content-Type",
+        },
+        AllowCredentials: false,
+    },))
 
     // ルーティング
     router.POST("/api/registUser",controller.RegistUserByApi)
